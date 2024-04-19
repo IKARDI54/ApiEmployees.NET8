371f78a3755393a21bae548fa5edb6cfc36fdd77
IKARDI54 <israel_sanchez_torrero@hotmail.es>
2024-04-19 16:00:09 +0200

    subida del proyecto completo

---
+++ WebAppemployees/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppemployees.Models;
+
+namespace WebAppemployees.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+
+        }
+
+        public DbSet<Employee> Employees{  get; set; }
+
+    }
+}
---
+++ WebAppemployees/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppemployees.Models
+{
+    public class Employee
+    {
+
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required int Age { get; set; }
+
+    }
+}
